1c6f4dc0018d76007a7b708ad0b56e8bfcdb2034
Guney Unay <guneymarketing17@hotmail.com>
2023-11-30 17:49:12 +0300

    added texterea

--- translate-app/containers/HomeContainer/index.tsx
+++ translate-app/containers/HomeContainer/index.tsx
@@ -16,13 +16,14 @@ function HomeContainer() {
         return { ...button, isActive: false };
       }
     });
-
     setButtons(updatedButtons);
+    const filteredData = buttons.find((button) => button.id === clickedId);
+    console.log(filteredData);
   };
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-4 px-6">
-      <div className="bg-[#212936cc] bg-opacity-80 w-full h-[300px] border border-[#4D5562] border-solid rounded-3xl p-6">
+      <div className="bg-[#212936cc] bg-opacity-80 w-full  border border-[#4D5562] border-solid rounded-3xl p-6">
         <div className="flex gap-3 items-center border-b border-[#4d5562] border-solid pb-4">
           <Button title={"Detect Language"} type="Primary" />
           {buttons.map((button) => (
@@ -36,16 +37,35 @@ function HomeContainer() {
             />
           ))}
         </div>
-        <div>content</div>
-        {/* <input
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        /> */}
+        <div className=" w-full h-[200px] ">
+          <textarea
+            className="w-full h-full bg-transparent resize-none outline-none text-white py-6"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
       </div>
-
-      <div className="bg-[#212936cc] bg-opacity-80 w-full h-[300px] border border-[#4D5562] border-solid rounded-3xl p-6">
-        onebox
+      {/* ıkıncı box */}
+      <div className="bg-[#212936cc] bg-opacity-80 w-full  border border-[#4D5562] border-solid rounded-3xl p-6">
+        <div className="flex gap-3 items-center border-b border-[#4d5562] border-solid pb-4">
+          {buttons.map((button) => (
+            <Button
+              title={button.title}
+              key={button.id}
+              id={button.id}
+              type={"Primary"}
+              isActive={button.isActive}
+              onClick={() => handleClick(button.id)}
+            />
+          ))}
+        </div>
+        <div className=" w-full h-[200px]  ">
+          <textarea
+            disabled
+            className="w-full h-full bg-transparent resize-none outline-none text-white py-6  "
+            value={value}
+          />
+        </div>
       </div>
     </div>
   );
