487bf879db1fe038e72a145356567ff284261a93
Guney Unay <guneymarketing17@hotmail.com>
2023-12-05 23:47:15 +0300

    style update

--- simple-coffee-listing/components/index.jsx
+++ simple-coffee-listing/components/index.jsx
@@ -2,8 +2,21 @@ import React from "react";
 
 function CardComponent() {
   return (
-    <div className="bg-[#1B1D1F] -mt-20 z-10 w-[80%] mx-auto bg-vector py-10 px-6 bg-no-repeat bg-right  ">
-      card component
+    <div className="bg-[#1B1D1F] -mt-20 z-10 w-[80%] max-w-[1200px] mx-auto  py-10 px-6 flex flex-col items-center rounded-md ">
+      <div className="flex flex-col gap-4 bg-vector bg-no-repeat bg-right-top max-w-[510px] ">
+        <h2 className="text-[#FEF7EE] text-3xl font-semibold text-center ">
+          Our Collection
+        </h2>
+        <p className="text-base text-[#6F757C] font-medium text-center">
+          Introducing our Coffee Collection, a selection of unique coffees from
+          different roast types and origins, expertly roasted in small batches
+          and shipped fresh weekly.
+        </p>
+        <div className="mx-auto">
+          <button>All Products</button>
+          <button>Available Now</button>
+        </div>
+      </div>
     </div>
   );
 }
