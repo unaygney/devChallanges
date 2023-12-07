2b831632946f278e453f62bf9f21f18fbc07c3c3
Guney Unay <guneymarketing17@hotmail.com>
2023-12-07 20:32:15 +0300

    added minor style bugs

--- simple-coffee-listing/Containers/HomePageContainer/index.jsx
+++ simple-coffee-listing/Containers/HomePageContainer/index.jsx
@@ -5,7 +5,7 @@ import CardComponent from "@/components/CardComponent";
 function HomePageContainer() {
   return (
     <div className="w-full h-full flex flex-col pb-20 ">
-      <div className="w-full h-[300px] relative ">
+      <div className="w-full relative " style={{ height: "300px" }}>
         <Image
           src={HeroImage}
           alt="Hero Image"
